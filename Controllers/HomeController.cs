26b21fef787db29e137c4b96d06802afff8de18b
Sercan Yüksel <72672537+SercanYuksel1@users.noreply.github.com>
2023-07-17 14:48:46 +0300

    url fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -110,7 +110,7 @@ public class HomeController : Controller
     }
 
     [ApiExplorerSettings(IgnoreApi = true)]
-    [HttpPost]
+    [HttpPost("Token")]
     public async Task<IActionResult> Token([FromBody]TokenRequest tokenRequest)
     {
         if(tokenRequest.grant_type == "authorization_code")
