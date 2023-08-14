5b39cae70272411f75f339ce60bd8be44d14c22a
Sercan Yüksel <srcanyuksel@gmail.com>
2023-08-14 12:00:20 +0300

    response added

--- amorphie.token/Modules/GenerateTokens.cs
+++ amorphie.token/Modules/GenerateTokens.cs
@@ -21,7 +21,9 @@ public static class GenerateTokens
         [FromServices] IAuthorizationService authorizationService
         )
         {
-            
+            var transitionName = body.GetProperty("LastTransition").ToString();
+            dynamic bodyData = body.GetProperty("TRX-start-password-flow").GetProperty("Data");
+
             var requestBodySerialized = body.GetProperty("TRX-start-password-flow").GetProperty("Data").GetProperty("entityData").ToString();
             
             TokenRequest requestBody = JsonSerializer.Deserialize<TokenRequest>(requestBodySerialized,new JsonSerializerOptions
@@ -47,10 +49,10 @@ public static class GenerateTokens
 
             if(result.StatusCode == 200)
             {
-                dynamic variables = new ExpandoObject();
-                variables.status = true;
-                variables.tokenResponse = result.Response;
-
+                bodyData.additionalData = result.Response;
+                dynamic variables = new Dictionary<string,dynamic>();
+                variables.Add("status" ,true);
+                variables.Add($"TRX-{transitionName}",bodyData);
                 return Results.Ok(variables);
             }
             else
