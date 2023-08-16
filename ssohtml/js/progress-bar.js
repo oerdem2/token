446bc1061f83228350924743ccae9b6f2db8bcd7
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2023-08-16 16:25:50 +0300

    html init

---
+++ ssohtml/js/progress-bar.js
@@ -0,0 +1,21 @@
+// on page load...
+moveProgressBar();
+
+// SIGNATURE PROGRESS
+function moveProgressBar() {
+  let currentSec = 0;
+  let targetSecs = 10;
+
+  let interval = setInterval(() => {
+    currentSec += 1;
+
+    if (currentSec <= targetSecs) {
+      let percent = currentSec / targetSecs * 100;
+      document.getElementsByClassName('progress-bar')[0].style.width = `${percent}%`;
+      document.getElementsByClassName('progress-text')[0].innerHTML = targetSecs - currentSec;
+    } else {
+      clearInterval(interval);
+      document.getElementsByClassName('progress-wrap')[0].style.display = 'none';
+    }
+  }, 1000);
+}
