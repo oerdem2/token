88fe4b9f76c311cd0ae462860cad07d42a815074
Sercan Yüksel <srcanyuksel@gmail.com>
2024-02-13 15:34:07 +0300

    fix

--- amorphie.token.data/DatabaseContext.cs
+++ amorphie.token.data/DatabaseContext.cs
@@ -26,9 +26,8 @@ namespace amorphie.token.data
         {
             
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.LogTo(Console.WriteLine);
         
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Logon>()
