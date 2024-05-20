0aef27cd89f569a579614320c10833375e07b95a
Sercan Yüksel <srcanyuksel@gmail.com>
2024-05-20 13:41:39 +0300

    Collection Users Endpoint

--- amorphie.token.core/Models/Collection/Role.cs
+++ amorphie.token.core/Models/Collection/Role.cs
@@ -2,13 +2,9 @@ namespace amorphie.token.core.Models.Collection
 {
     public enum Role
     {
-        Yonetici = 10,
-        Yetkili_Yardimcisi = 20,
-        Tahsilat_Sorumlusu = 30,
-        Birim_Yonetici = 40,
-        Veri_Analitik_Modelleme_Yoneticisi = 50,
-        Veri_Analitik_Modelleme_Yetkilisi = 60,
-        Kıdemli_Destek_Hizmet_Gorevlisi = 70,
-        Yetkili = 80
+        Admin = 10,
+        Manager = 20,
+        TeamLeader = 30,
+        Agent = 40
     }
 }
