20933d1a9183231bc9ccadf879163c9376853477
Sercan Yüksel <srcanyuksel@gmail.com>
2024-06-05 15:04:19 +0300

    fix

--- amorphie.token/Services/Authorization/AuthorizationService.cs
+++ amorphie.token/Services/Authorization/AuthorizationService.cs
@@ -117,7 +117,8 @@ public class AuthorizationService : ServiceBase, IAuthorizationService
                 Subject = request.User ?? null,
                 ConsentId = request.ConsentId?.ToString(),
                 Nonce = request.Nonce,
-                State = request.State
+                State = request.State,
+                Profile = request.Profile
             };
 
             var code = await GenerateAuthorizationCode(authCode);
